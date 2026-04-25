e95e80c95399796710ef6680fa73628b6c4c9a2f
Aeirya Mohammadi <amohammadi001@u040233.si.ehu.es>
2026-04-25 16:01:54 +0200

    add download script for dataset in gdrive

---
+++ download.py
@@ -0,0 +1,58 @@
+import gdown
+from gdown.exceptions import FileURLRetrievalError
+from pathlib import Path
+
+
+OTHER_FOLDERS = {
+    'CORPUS': '1f72Z6E-CV8OrrU4qAw-55TFeJu6pMBzf',
+    'high_resource': '1yC9XEe4gZEZC9nx9pPtXZ2u85eJeblSF',
+}
+
+HI_RES = {
+    "arb-eng": "1Y9-moYj9RjP1HusJxTubPyXCUfhed8hV",
+    "deu-eng": "1tk5ttp6jMs7x-01HJtj4Owc_Fq35FPw5",
+    "ell-eng": "1iVF-ms8NjUm85Qhg2rotDQQHu2d4fD27",
+    "fin-eng": "1mbOkZ3yXHjpo0h8-X_GsKfOSg45nrG4t",
+    "hin-eng": "1JD8iqj6nxcfxqJEMAN0NqTA6GTunu7y-",
+    "hun-eng": "1pPnI7mm4KsIA1fDdG0cp6fKKSl7qZd20",
+    "ind-eng": "1QR14QvtEbVHnbMhUo6hCDsc9TCphUY_0",
+    "kor-eng": "1HxEqF_12LkXpwmJhT5M2S4qUVCHKirJV",
+    "pes-eng": "1FT8Z4pAww-wPudLlYTBjrZ1InX_XC4ur",
+    "rus-eng": "1gT087NXGZk_yocHJ-Gs7i-hzTfC5pNDd",
+    "spa-eng": "159gX_UJCz5l3k3n-sd6xngxkgm9jnuUw",
+    "tha-eng": "1L1h1YnQxHTobB_Yv-Rij_h-DdQvr2YmO",
+    "tur-eng": "1U5hoyafo2w7tjyJ6zW0FrrUltZUsolRb",
+    "vie-eng": "1TDlL0P_USHV9oBMWwLdyv51nTyvdt3Dx",
+}
+FOLDERS = HI_RES
+
+OUT_DIR = Path("corpus/high_resource")
+OUT_DIR.mkdir(exist_ok=True)
+
+failed = []
+
+for lang, folder_id in FOLDERS.items():
+    url = f"https://drive.google.com/drive/folders/{folder_id}"
+    out = OUT_DIR / lang
+    out.mkdir(exist_ok=True)
+
+    try:
+        gdown.download_folder(
+            url=url,
+            output=str(out),
+            quiet=False,
+            resume=True,
+            skip_download=False,
+        )
+    except FileURLRetrievalError as e:
+        print(f"SKIPPED blocked folder/file in {lang}")
+        failed.append((lang, folder_id, str(e)))
+    except Exception as e:
+        print(f"FAILED {lang}: {e}")
+        failed.append((lang, folder_id, str(e)))
+
+
+print("\nDone.")
+print("Failed folders:")
+for lang, folder_id, err in failed:
+    print(f"{lang}: {folder_id}")
